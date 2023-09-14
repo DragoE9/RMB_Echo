2d9088814d57e3a0a121fe410458b89aa8b60a80
DragoE9 <131825719+DragoE9@users.noreply.github.com>
2023-09-14 15:02:53 -0600

    Added multi-channel support and config file

--- RMB_Echo.py
+++ RMB_Echo.py
@@ -2,21 +2,36 @@ import nationstates
 import time
 import discord
 import re
+import tomli
 
+#Import settings from config
+with open ("config.toml","rb") as config_file:
+    settings = tomli.load(config_file)
 #Setup
-user = input("Enter User Name (Main Nation): ")
+user = settings["user_agent"]
 api = nationstates.Nationstates(user + "Running RMB Echo by DragoE")
-targ_regions = (input("Enter Regions to Watch, Seperated by Commas (EX: Conch Kingdom,Lands End): ")).lower().strip().replace(" ","_").split(",")
+targ_regions = settings["regions"]
 previous_messages = []
 for element in targ_regions:
     previous_messages.append([])
-requested_sleep = int(input("Enter the number of seconds between checks (minimum 100):"))
+requested_sleep = settings["refresh_rate"]
 if requested_sleep < 100:
     real_sleep = 100
 else:
     real_sleep = requested_sleep
-url = input("Enter your Webhook URL: ")
-webhook = discord.SyncWebhook.from_url(url)
+url = settings["webhooks"]
+if settings["mode"] == "single":
+    webhook = discord.SyncWebhook.from_url(url[0])
+    print("Setup Complete")
+elif settings["mode"] == "multi":
+    if len(targ_regions) == len(url):
+        print("Setup Complete")
+    else:
+        print("Error: Detected Mismatch Between Webhooks and Regions")
+        quit()
+else:
+    print("Error: Unkown Mode")
+    quit()
 
 #Loop Section
 while True:
@@ -26,7 +41,6 @@ while True:
         #Call to the API, retreive messages
         api_region = api.region(current_region)
         api_messages = (api_region.messages)["post"]
-        print(previous_messages)
         for post in api_messages:
             #Check that the post is new
             if post["id"] not in previous_messages[i]:
@@ -46,8 +60,13 @@ while True:
                 else:
                     final_msg = pretty_message
                 #Send it off to the Webhook
-                webhook.send("# {} in {} said:\n{}".format(pretty_name_nation,pretty_name_region,final_msg))
-                previous_messages[i].append(post["id"])
+                if settings["mode"] == "single":
+                    webhook.send("# {} in {} said:\n{}".format(pretty_name_nation,pretty_name_region,final_msg))
+                    previous_messages[i].append(post["id"])
+                else:
+                    webhook = discord.SyncWebhook.from_url(url[i])
+                    webhook.send("# {} in {} said:\n{}".format(pretty_name_nation,pretty_name_region,final_msg))
+                    previous_messages[i].append(post["id"])
             #Garbage collection to prevent previous messages from getting unecessarily big
             if len(previous_messages[i]) > 15:
                 previous_messages[i].pop(0)
